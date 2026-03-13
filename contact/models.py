12904dfa9436e89fbfcb378766f625e18620a835
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2026-03-13 06:44:32 +0200

    added Contactus

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) # وقت إرسال الرسالة
+
+    def __str__(self):
+        return f"Message from {self.name}"
+    
+    class Meta:
+        verbose_name = "رسالة تواصل"
+        verbose_name_plural = "جميع رسائل التواصل"
