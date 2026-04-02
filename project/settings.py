4fefb56c64246a7843df7b1e98f551b7d13ff187
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2026-04-02 12:20:32 +0200

    added seo

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gxf@7$gtaqksc^2k^pd(hmwgnkbl3e12@h6n4ww0s+nuv0=!^('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = [
     "door-eg-production.up.railway.app",
     "door-eg.com",
